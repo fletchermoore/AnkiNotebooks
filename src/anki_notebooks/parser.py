460287f094b20bed61901abb738cc86d5a2beec6
Fletcher Moore <fletcher@Fletchers-MacBook-Pro.local>
2019-03-02 00:41:55 -0500

    hackish approach to namespace

--- src/anki_notebooks/parser.py
+++ src/anki_notebooks/parser.py
@@ -1,5 +1,6 @@
 from zipfile import ZipFile, is_zipfile
 import xml.etree.ElementTree as ET
+import re
 
 
 # returns string representing content of docx internal file document.xml or None on failure
@@ -27,22 +28,41 @@ def addLine(logStr, msg):
     return logStr + msg + "\n"
 
 
+# stolen from stackoverflow
+# fuck xml
+def getNamespace(element):
+    m = re.match('\{.*\}', element.tag)
+    return m.group(0) if m else ''
+
+
+# does not work; delete later
+def printNamespace(xmlStr):
+    root = ET.fromstring(xmlStr)
+    # assume we are the document tag
+    print("Finding namespace...")
+    print(root.attrib)
+    #print(root.get('xmlns:w'))
+
+
 # expects xml string read from docx file
 # return parsed content string (currently nebulus)
 def parseXml(xmlStr):
     root = ET.fromstring(xmlStr)
     # how to check this is valid?
     
+    namespace = ""
     out = ""
     for child in root:
+        namespace = getNamespace(child)
         out = addLine(out, child.tag) # body tag
         for grandchild in child: 
             # p tags
-            paragraphText = parseParagraph(grandchild)
+            paragraphText = parseParagraph(grandchild, namespace)
             out = addLine(out, paragraphText)
     return out
 
 
+# current unused
 # expects namespaced tag name like {really-long-string}p
 # returns unnamespaced tag name like p
 def shortTag(nsTag):
@@ -52,26 +72,47 @@ def shortTag(nsTag):
 
 # expects xml node representing w:r element
 # returns string of text contents
-def rText(rXml):
+def rText(rXml, namespace):
     # as far as I know, possible children include w:t and w:lastRenderedPageBreak
     text = ""
+    tTag = namespace + 't'
     for child in rXml:
-        if shortTag(child.tag) == 't':
+        if child.tag == tTag:
             text += child.text
     return text
+
+# except xml node for w:Pr
+# returns indent level or None
+def parseIndentLevel(prXml, namespace):
+    numPrTag = namespace + 'numPr'
+    ilvlTag = namespace + 'ilvl'
+    valAttr = namespace + 'val'
+    for child in prXml:
+        if child.tag == numPrTag:
+            for grandchild in child:
+                if grandchild.tag == ilvlTag:                    
+                    return grandchild.get(valAttr)
+    return None
 
 
 # expects ET xml for w:p tag
 # returns text content
-def parseParagraph(pChildXml):
-    preamble = "presumed w:p contents: "
+def parseParagraph(pChildXml, namespace):
     textContent = ""
+    indentLevel = None
+    rTag = namespace + 'r'
+    pPrTag = namespace + 'pPr'
     for child in pChildXml:
-        tag = shortTag(child.tag)
+        tag = child.tag
         # either w:Pr, w:r, w:proofErr, w:gramErr
-        if tag == 'r': 
-            textContent += rText(child)
+        if tag == rTag: 
+            textContent += rText(child, namespace)
+        elif tag == pPrTag:
+            indentLevel = parseIndentLevel(child, namespace)
         # ignore all other paragraph children for now
-    return preamble + textContent
+    if indentLevel == None:
+        return textContent
+    else:
+        return str(indentLevel) + ': ' + textContent
 
 
--- src/anki_notebooks/parser_tests.py
+++ src/anki_notebooks/parser_tests.py
@@ -2,19 +2,30 @@
 # just so I don't have to launch Anki as I code
 
 
-from parser import unzipDoc, parseXml
+from parser import unzipDoc, parseXml, printNamespace
 import os
-#testdir = os.path.dirname(__file__)
-#srcdir = '../antigravity'
-#sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
-def testParser():
+def getPath():
     currentDir = os.path.dirname(__file__)
     path = os.path.abspath(os.path.join(currentDir, '../../sample_docs/spine 1.docx'))
+    return path
 
+def testFindNs(xmlContentStr):
+    printNamespace(xmlContentStr)
+
+
+def testParser():
+    path = getPath()
     xmlContentStr = unzipDoc(path)
     if xmlContentStr != None:
-        log = parseXml(xmlContentStr)
-        print(log)
+        testMainParser(xmlContentStr)
+        #testFindNs(xmlContentStr)
+
+def testMainParser(xmlContentStr):
+    log = parseXml(xmlContentStr)
+    print(log)
+
+
+
 
 testParser()
