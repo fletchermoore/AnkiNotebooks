c54307dba3392cd7fdae2d88073f66d2cc2b4b11
Fletcher Moore <fletcherbmoore@gmail.com>
2019-08-12 17:21:46 -0400

    added reverse markup

--- src/anki_notebooks/cards.py
+++ src/anki_notebooks/cards.py
@@ -9,9 +9,27 @@ def genCards(pathList):
         if length > 1:
             front = ': '.join(path[:length-1])
             back = path[-1]
-            cards.append((front, back)) 
+            cards.append((front, back))
+    cards = reverseMarkedCards(cards)
     cards = consolidateCards(cards)
     return cards
+
+
+# expects list of tuples ('front', 'back')
+# swaps back and front if the back-string starts with '<<'
+# returns same format
+def reverseMarkedCards(cards):
+    updatedCards =[]
+    for card in cards:
+        front = card[0]
+        back = card[1]
+        if back.find('<<') == 0: # if back starts with <<
+            newFront = back[2:].lstrip()
+            newBack = front
+            updatedCards.append((newFront, newBack))
+        else:
+            updatedCards.append(card)
+    return updatedCards
 
 
 # expects a list of tuples
